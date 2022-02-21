c04e8ef57d2674cf736f473a6b640c9f5b7a0fb9
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2022-02-20 19:04:53 -0600

    subiendo los primeros archivos

---
+++ index.php
@@ -0,0 +1,200 @@
+<!-- 
+    -Portafolio 
+    -Angela Garcia #22869
+    -fecha de ultima modificacion: 20/02/2022
+-->
+
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="../.universidad/2022/introduccion a computer science/portafolio/css/estilo2.css">
+
+    <title>Portafolio</title>
+  </head>
+  <body>
+      
+      <script><?php include("navbar.html");?></script>
+    
+
+<!--------------------------------------------- Slider / carrusel -------------------------------------------->
+
+    <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+      <!-- Indicadores -->
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+
+      <!-- Imagenes -->
+      <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="1000">
+          <img src="../proyecto/img/carrusel3.jpg" class="d-block w-100 h-90" alt="img1">
+          <div class="carousel-caption d-none d-md-block">
+            <font color="white" SIZE=40 Face="impact">
+            Creciendo Juntos para un mejor futuro
+          
+          </div></font>
+        </div>
+
+        <div class="carousel-item" data-bs-interval="2000">
+          <img src="../proyecto/img/carrusel2.jpg" class="d-block w-100" alt="img2">
+          <div class="carousel-caption d-none d-md-block">
+            
+          </div>
+        </div>
+
+        <div class="carousel-item" data-bs-interval="2000">
+          <img src="../proyecto/img/carrusel4.jpg" class="d-block w-100" alt="img3" >
+          <div class="carousel-caption d-none d-md-block">
+            
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+
+    <!-- -------------------------------              Main                  ------------------------------------------------->
+    <main>
+      <!--  Espacios -->
+      <section class="altura-a-b">
+
+        <!-- Quien Soy -->
+      <section id="QuienSoy">
+        <div class="container">
+          <div class="row">
+
+            <div class="col-12 col-sm-6">
+              <img class="img-fluid shadow-lg" src="../proyecto/img/quienes1.jpg" alt="" width="400" height="300"><br><br><br><br><br>
+            </div>
+
+            <div class="col-12 col-sm-6 text-center">
+              <h2>¿Quiénes Somos?</h2>
+              <p>
+                El colegio Concepción es un colegio que tiene como modelo la innovación. Y busca estudiantes a los cuales tengan
+                 varios talentos, habilidades y que quieran ser agentes de cambio para el país. Nosotros ofrecemos una educación
+                  de calidad, con distintos enfoques como en la tecnología, artes y ciencias, donde nuestros estudiantes crecen en
+                   un buen ambiente y muy bien preparados para salir adelante.
+              </p>
+            </div>
+
+        <br>
+        <!-- Estudios -->
+      <section id="Estudio">
+        <div class="container">
+          <div class="row">
+
+            <div class="col-12 col-sm-6">
+              <img class="img-fluid shadow-lg" src="../proyecto/img/Chonita.jpg" alt="" width="400" height="300"><br><br><br><br><br>
+            </div>
+
+            <div class="col-12 col-sm-6 text-center">
+              <h2>Historia</h2>
+              <p>
+                El Colegio Concepción, nace el 13 de septiembre de 2021, fecha en la que se le dio inicio a las inscripciones,
+                 para estar listos comenzando el 2022. Su fundadora Angela García, inspirada al viajar y conocer a Concepción Ramírez
+                  la mujer que tiene su rostro en la moneda de 25 centavos de Guatemala, también conocida como Doña Chonita.
+                   Al conocerla le contó su historia y que ella era analfabeta, contándole que ella consideraba que los estudios
+                    son muy importantes, además de representar una parte de la identidad y cultura guatemalteca, pero que ella 
+                    nunca tuvo la oportunidad para estudiar, sin embargo, ella si le dio la oportunidad de estudiar a sus hijos.
+                     Entonces al regresar de Santiago Atitlán se conmovió por la historia de Doña Chonita, para poder hacer cambios
+                      en el país, creando un colegio en donde se pueda educar de una buena manera, donde los estudiantes crezcan en
+                       un ambiente recreativo y mientras aprenden acerca de la conciencia social del país.
+              </p>
+            </div>
+
+          </div>
+        </div>
+      </section>
+
+
+      
+        <!--  Intereses -->
+        <section id="Intereses">
+            <div class="container">
+              <div class="row">
+
+                <div class="col-12 col-sm-6 text-center">
+                
+                  <img class="img-fluid shadow-lg" src="../proyecto/img/wallpaper8.jpg" alt="img4" width="500" height="400"><br>
+                  <br><h2>Intereses</h2>
+                  <p> 
+                    Educar a los estudiantes para que puedan aprender acerca de la libertad, creatividad, independencia,
+                     trabajo en grupo y valores para que se puedan desarrollarse en un ambiente positivo.
+
+                  </p>
+                </div>
+               
+              </div>
+            </div>
+        </section>
+
+      <!--  Carrera -->
+      <section id="Carrera">
+        <div class="container">
+          <div class="row">
+
+            <div class="col-12 col-sm-6 text-center">
+            
+              <img class="img-fluid shadow-lg" src="../proyecto/img/wallpaper8.jpg" alt="img4" width="500" height="400"><br>
+              <br><h2>Interes al tomar la carrera en Ingeniería en computación</h2>
+              <p> 
+                Educar a los estudiantes para que puedan aprender acerca de la libertad, creatividad, independencia,
+                 trabajo en grupo y valores para que se puedan desarrollarse en un ambiente positivo.
+
+              </p>
+            </div>
+           
+          </div>
+        </div>
+    </section>
+
+
+
+    </main>
+
+    <!-- -------------------------------              footer                  ------------------------------------------------->
+    <footer>
+      <section>
+        <div class="container">
+          <div class="row">
+
+            <div class="col-12 col-sm-6">
+            
+              <img src="../proyecto/img/logo1.jpg" alt="logo">              
+            </div>
+            <div class="col-12 col-sm-6 text-left">
+              <br><br><br><br><br>
+              <h6>© 2022 Angela García. Todos los derechos reservados</h6>
+              
+            </div>
+
+        
+          </div>
+        </div>
+    </section>
+     
+      
+      
+    </footer>
+
+    <!-- bootstrap xd -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+
+  </body>
+</html>
